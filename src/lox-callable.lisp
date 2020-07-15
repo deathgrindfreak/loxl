b3bc058d853bb3ece7b94cc839811c3f28b635e6
Cooper Bell <john.cooper.bell@gmail.com>
2020-07-15 16:02:34 -0500

    Add lox-callable class

---
+++ src/lox-callable.lisp
@@ -0,0 +1,32 @@
+(in-package :interpreter)
+
+(defclass lox-callable () ())
+
+(defgeneric call-fun (lox-callable interpreter arguments))
+
+(defgeneric arity (lox-callable))
+
+
+(defclass lox-function (lox-callable)
+  ((declaration :initarg :declaration
+                :reader declaration)))
+
+(defmethod arity ((l lox-function))
+  (with-slots ((params ast::params)) (declaration l)
+    (length params)))
+
+(defmethod call-fun ((l lox-function) (i interpreter) arguments)
+  (let ((env (make-instance 'environment :enclosing (globals i))))
+    (with-slots ((params ast::params) (body ast::body)) (declaration l)
+      (loop for param in params
+            for arg in arguments
+            do (define env (lexeme param) arg t))
+      (execute-block i body env))))
+
+;; Native clock function
+(defclass clock (lox-callable) ())
+
+(defmethod arity ((l clock)) 0)
+
+(defmethod call-fun ((l clock) (i interpreter) arguments)
+  (/ (get-universal-time) 1000.0))
